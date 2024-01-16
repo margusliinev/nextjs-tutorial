7450c30882746aec3f165cd0a0292e8b8c7e872d
Margus Liinev <margus.liinev@gmail.com>
2024-01-16 21:15:32 +0200

    completed chapter 12

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { generatePagination } from '@/app/lib/utils';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
-import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
